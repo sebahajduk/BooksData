5b7abac507ba044933ef5f2fa74fd0fd41fb0855
Sebastian Hajduk <shajduk0@gmail.com>
2022-05-26 19:56:58 +0200

    Initial Commit

---
+++ BooksData/BooksDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksDataApp.swift
+//  BooksData
+//
+//  Created by Sebastian Hajduk on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BooksDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
